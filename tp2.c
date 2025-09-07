44d4ec7acb0de00611738b31fc161ac74568efcc
Hamones <63001546+Hamones@users.noreply.github.com>
2025-09-07 17:23:27 -0300

    Update tp2.c

--- tp2.c
+++ tp2.c
@@ -7,6 +7,7 @@
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
 #include <stdlib.h>
+#include "racional.h"
 
 /* coloque aqui as funções auxiliares que precisar neste arquivo */
 
@@ -16,32 +17,48 @@ int main ()
   /* a completar! */
   /*defina um vetor para até 100 números racionais
   */
-struct racional vetor[100];
+  int tam,n; 
+  struct racional vetor[tam];
 
   /*
   leia um valor n tal que 0 < n < 100
   */
-  for (i= 0; i < n; i++)
-    scanf("%d",&n);
-    v[i].num -> n;
-    scanf("%d",&n);
-    v[i].dem
-    
-    
+  //opção 1
+  for (i= 0; i < (tam-1) ; i++)
+    {
+      v[i].num -> 0;
+      v[i].den -> 0;
+    }
+  //opção 2
+    for (i= 0; i < (tam-1) ; i++)
+    {
+      criar_r(
+    }
   /* 
   preencha o vetor com n númerros racionais lidos da entrada
   (leia o numerador e o denominador de cada racional
   */
-  vetor = &script;
+  for (i= 0; i < (tam-1) ; i++)
+    {
+      scanf("%d",&n);
+      v[i].num -> n;
+      scanf("%d",&n);
+      v[i].den -> n;
+    }
   /*
   imprima "VETOR= " e o conteúdo do vetor lido
   */
-  printf("VETOR= ",&vetor)
+  for (i= 0; i < (tam-1) ; i++)
+    {
+      printf("VETOR= ");
+      imp
+    }
+  
   /*
   elimine deste vetor os elementos inválidos
   imprima "vet =" e o conteúdo do vetor resultante
   */
-  
+    
   /*
   calcule a soma de todos os elemetnos do vetor
   imprima "SOMA =" e a soma calculada acima
