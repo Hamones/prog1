897e922ae283e31a074502b55d6f2e8238d04ea0
Hamones <63001546+Hamones@users.noreply.github.com>
2025-09-07 17:42:43 -0300

    Update tp2.c

--- tp2.c
+++ tp2.c
@@ -17,9 +17,8 @@ int main ()
   /* a completar! */
   /*defina um vetor para até 100 números racionais
   */
-  int tam,n,num,den;
+  int tam,n,num,den,i;
   struct racional vetor[tam];
-
   /*
   leia um valor n tal que 0 < n < 100
   */
@@ -33,7 +32,7 @@ int main ()
     for (i= 0; i < (tam-1) ; i++)
     {
       scanf("%d %d,&num,&den);
-      criar_r(num,den);
+      criar_r(v[i]);
     }
   /* 
   preencha o vetor com n númerros racionais lidos da entrada
@@ -41,9 +40,9 @@ int main ()
   */
   for (i= 0; i < (tam-1) ; i++)
     {
-      scanf("%d",&n);
+      scanf("%d",&n); //adição de script1
       v[i].num -> n;
-      scanf("%d",&n);
+      scanf("%d",&n); //adição de script1
       v[i].den -> n;
     }
   /*
@@ -52,21 +51,36 @@ int main ()
   for (i= 0; i < (tam-1) ; i++)
     {
       printf("VETOR= ");
-      imprime_r(
+      imprime_r(v[i]); //deve imprimir "NaN"
     }
   
   /*
   elimine deste vetor os elementos inválidos
   imprima "vet =" e o conteúdo do vetor resultante
   */
+    for (i= 0; i < (tam-1) ; i++)
+    {
+      if ( valido_r(v[i]) = 1)
+        v[i] = v[++i]; //funciona em v[tam]?
+      
+      printf("VETOR= ");
+      imprime_r(v[i]); //deve imprimir "NaN"
+    }
     
   /*
   calcule a soma de todos os elemetnos do vetor
   imprima "SOMA =" e a soma calculada acima
   */
+  for (i= 0; i < (tam-1) ; i++)
+    {
+      if ( valido_r(v[i]) = 1)
+        v[i] = 0;
+      printf("VETOR= ");
+      imprime_r(v[i]); //deve imprimir "NaN"
+    }
+  printf("%d", &soma);
   /*
   retorne 0
-  
   */
 
   return (0) ;
