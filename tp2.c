ee5e614ec937197138b4a52b3a306ab533c58d19
Hamones <63001546+Hamones@users.noreply.github.com>
2025-09-08 20:17:09 -0300

    Update tp2.c
    linux: iImprimir a primeira linha sem se preocupar com a validade.

--- tp2.c
+++ tp2.c
@@ -1,4 +1,4 @@
- Tipos Abstratos de Dados - TADs
+/* Tipos Abstratos de Dados - TADs
  * Arquivo do programa principal, que usa o TAD racional.
  * Feito em 24/09/2024 para a disciplina CI1001 - Programação 1.
 */
@@ -17,22 +17,28 @@ int main ()
   /*defina um vetor para até 100 números racionais
   */
   srand(0);
-  int tam,n,num,den,i;
-  
-  struct racional vetor[tam];
-  struct racional *soma;
+  int tam,i;
+  long int n;
+
+  scanf("%d",&tam);
+
+  struct racional v[tam];
+  struct racional soma;
   /*
   leia um valor n tal que 0 < n < 100
   */
   //opção 1:
-	scanf("%d",&tam);
-	if (tam < 0 || tam >100)
-		return1;
-  for (i= 0; i <) (tam-1) ; i++)
+	
+	/*if (tam < 0 || tam >100)
+		return 1;
+  */
+  
+  for (i= 0; i < tam ; i++)
     {
-      v[i].num -> 0;
-      v[i].den -> 0;
+      v[i].num = 0;
+      v[i].den = 0;
     }
+  i=0;
   /*opção 2:
     for (i= 0; i < (tam-1) ; i++)
     {
@@ -43,44 +49,49 @@ int main ()
   preencha o vetor com n númerros racionais lidos da entrada
   (leia o numerador e o denominador de cada racional
   */
-  for (i= 0; i < (tam-1) ; i++)
-    {
-      scanf("%d",&n); //adição de script1
-      v[i].num -> n;
-      scanf("%d",&n); //adição de script1
-      v[i].den -> n;
+  for (i= 0; i < tam ; i++)
+    { printf("scanf funcionou deve funcionar...");
+      printf("\n");
+      scanf("%ld",&n); //adição de script1
+      printf("scanf funcionou");
+      printf("\n");
+      v[i].num = n;
+      scanf("%ld",&n); //adição de script1
+      v[i].den = n;
     }
+  
   /*
   imprima "VETOR= " e o conteúdo do vetor lido
   */
-  for (i= 0; i < (tam-1) ; i++)
+  for (i= 0; i < tam-1 ; i++)
     {
-      printf("VETOR= ");
-      imprime_r(v[i]); //deve imprimir "NaN"
+      for (i= 0; i < tam ; i++)
+      //imprime_r(v[k]); //deve imprimir "NaN"
+      printf("%ld/%ld \|",v[i].num,v[i].den);
     }
   
   /*
   elimine deste vetor os elementos inválidos
   imprima "vet =" e o conteúdo do vetor resultante
-  */
-    for (i= 0; i < (tam-1) ; i++)
+      for (l= 0; l < (tam-1) ; l++)
     {
-      if ( valido_r(v[i]) = 1)
-        v[i] = v[++i]; //funciona em v[tam]?
+      if ( valido_r(v[l]) == 1)
+        v[l] = v[++l]; //funciona em v[tam]?
       
-      printf("VETOR= ");
-      imprime_r(v[i]); //deve imprimir "NaN"
+        printf("VETOR= ");
+      imprime_r(v[l]); //deve imprimir "NaN"
     }
-    
+    */
+
   /*
   calcule a soma de todos os elemetnos do vetor
   imprima "SOMA =" e a soma calculada acima
   */
-  soma_r(v[i],v[++i],soma);
-  printf("%d", &soma);
+  soma_r(v[i],v[++i],&soma);
+  printf("\n");
+  printf("valor da soma: %ld/%ld", soma.num,soma.den);
   /*
   retorne 0
   */
-
   return (0) ;
 }
