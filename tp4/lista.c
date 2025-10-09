d2d8703f400a092336fe40f96dc45ed1fce87b7f
Hamones <63001546+Hamones@users.noreply.github.com>
2025-10-09 16:22:25 -0300

    Add files via upload

--- tp4/lista.c
+++ tp4/lista.c
@@ -16,33 +16,119 @@ struct lista {
 /* ---------------------------------------------------------------------- */
 
 struct lista *lista_cria (){
+    struct fila *nova_lista malloc (sizeof(struct lista));
+
+    if (!nova_lista)
+        return 0;
+
+    memset (nova_lista, 0, sizeof(struct lista));
+
+    return nova_lista;
+
+    
 }
 
-void lista_destroi (struct lista **lista){
+void lista_destroi (struct lista **lista){ //o formato inicial preve *lista
+    if (!*lista)
+        return -1;
+    
+    struct nodo * aux;
+    for (int i = 0; i < *lista -> *tamanho; i++){
+        aux = lista->inicio;
+        lista->inicio = aux->prox;
+        free (aux);
+    }
+
+    free (lista);
+    return 0;
+
 }
 
 int lista_insere_inicio (struct lista *lista, int chave){
+    if (!lista)
+        return -1;
+
+    if (lista->tamanho == CAP)
+        return -2;
+
+    struct nodo *novo_nodo = malloc (sizeof(struct nodo));
+
+    if (!novo_nodo)
+        return -3;
+
+    memset (novo_nodo, 0, sizeof(struct nodo));
 }
 
 int lista_insere_fim (struct lista *lista, int chave){
+        if (!lista)
+        return -1;
+
+    if (lista->tamanho == CAP)
+        return -2;
+
+    struct nodo *novo_nodo = malloc (sizeof(struct nodo));
+
+    if (!novo_nodo)
+        return -3;
+
+    memset (novo_nodo, 0, sizeof(struct nodo));
 }
 
 int lista_insere_ordenado (struct lista *lista, int chave){
 }
 
 int lista_remove_inicio (struct lista *lista, int *chave){
+    if (! lista || !dado)
+        return -1;
+
+    if (!lista -> tamanho)
+        return -2;
+
+    struct nodo *aux = lista->inicio;
+    *dado = aux->valor;
+    lista->inicio = aux->prox;
+
+    lista->tamanho--;
+
+    if (!lista-tamanho)
+        lista-fim = 0;
+
+    free (aux);
+    return 0;
 }
 
 int lista_remove_fim (struct lista *lista, int *chave){
+    if (! lista || !dado)
+        return -1;
+
+    if (!lista -> tamanho)
+        return -2;
+
+    struct nodo *aux = lista->inicio;
+    *dado = aux->valor;
+    lista->inicio = aux->prox;
+
+    lista->tamanho--;
+
+    if (!lista-tamanho)
+        lista-fim = 0;
+
+    free (aux);
+    return 0;    
 }
 
 int lista_remove_ordenado (struct lista *lista, int chave){
 }
 
 int lista_vazia (struct lista *lista){
+    lista-<
 }
 
 int lista_tamanho (struct lista *lista){
+    if (!lista)
+        return -1;
+
+        return lista->tamanho;
 }
 
 int lista_pertence (struct lista *lista, int chave){
