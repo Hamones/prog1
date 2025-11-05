dc1885994e67c9e56c8b463761b784a5aa3c19a8
Hamones <63001546+Hamones@users.noreply.github.com>
2025-11-05 01:57:57 -0300

    Atualizar o theboys.c

--- theboys/theboys.c
+++ theboys/theboys.c
@@ -1,5 +1,5 @@
 // programa principal do projeto "The Boys - 2024/2"
-// Autor: xxxxx, GRR xxxxxx
+// Autor: Ramon Cesar Santos Alves, GRR 20204080
 
 // seus #includes vão aqui
 
@@ -10,7 +10,26 @@
 // programa principal
 int main ()
 {
-  // iniciar o mundo
+	/* testes iniciais.*/
+item com maior prioridade();
+
+iniciar evento com maior prioridade.
+gerar novos eventos na fila de prioridade.
+executar alterações no mundo.
+
+
+inicciar eventos pequenos.
+iniciar eventos grandes.
+iniciar missões.
+
+
+grau de descisão de cada agente frente a cada local 
+
+
+
+	
+
+  // iniciar o mundos
 
   // executar o laço de simulação
 
