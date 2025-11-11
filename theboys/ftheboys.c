28e38b0fba9140c19b9e6f9e03cc14e6b7a2b94f
Hamones <63001546+Hamones@users.noreply.github.com>
2025-11-11 16:23:52 -0300

    Implement main program structure for 'The Boys'
    Initial implementation of the main program for 'The Boys' project, including setup for heroes and bases, event handling, and simulation loop.

---
+++ theboys/ftheboys.c
@@ -0,0 +1,63 @@
+// programa principal do projeto "The Boys - 2024/2"
+// Autor: Ramon Cesar Santos Alves, GRR 20204080
+
+// seus #includes vão aqui
+#include "theboys.h"
+// seus #defines vão aqui
+
+// minimize o uso de variáveis globais
+
+// programa principal
+int main ()
+{
+
+
+  // iniciar o mundo
+	caracteŕisticas do mundo
+	
+/* Implementações Iniciasi.*/
+item com maior prioridade();
+
+Mundo - tamanho do mundo. Base e Altura.
+-Herói
+--criar vetor herois [];
+--posição(variável) == posiçao inicial (Rand(min max) //(x,y) que seria a posição aleatória. /* evitar e de criar o personagem dentro de uma base*/ caso for um local de base fazer o calculo novamente. 
+--habilidades == {h1,h2,h2,h4,h5,h6} //vetor simples.
+--experiência == aumenta quando a missão acontece. //poderíamos aumentar quando ele se desloca, mas ele também pode ser expulso da base.
+--deslocamento == tempo para a missão mais próxima.
+---verificar todas as bases -> escolher a mais próxima. 
+----não retornar para base anterior.
+---criar a função tempo que descide o tempo para chegar em outra base.
+----evitar conflitos com outras bases.
+--Morrer == v[H1] = NULL;
+h
+-Base
+--criar vetor base[].
+--posição (fixa) == posição inicial (Rand(min Max)) // (x,y) que seria a posição aleatória. /*evitar que uma missão fique dentro.
+--lista de heróis (o tamanho varia?) == 
+--lista de espera (fixo) == 
+--porteiro ==  
+
+
+  // executar o laço de simulação
+/*Eventos*/
+
+iniciar evento com maior prioridade.
+gerar novos eventos na fila de prioridade.
+executar alterações no mundo.
+
+iniciar eventos pequenos.
+- deslocamento do herói.
+iniciar eventos grandes.
+- tempo de permanência do herói
+--se agente sair = atualzar conjunto
+iniciar missões.
+- concluir a missão: verificar todos os conjuntos das bases.
+- decidir qual herói tomará a poção.
+
+  // destruir o mundo
+/*Liberar ponteiros:
+- Executar de funções destroi para todos os ponteiros.*/
+  return (0) ;
+}
+
