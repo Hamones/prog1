ea17cacc8a69efb93a8bc6827f5f4819e16941ce
Hamones <63001546+Hamones@users.noreply.github.com>
2025-11-18 14:36:06 -0300

    Refactor and clean up fprio.c implementation

--- theboys/fprio.c
+++ theboys/fprio.c
@@ -8,23 +8,6 @@
 #include <string.h>
 #include "fprio.h"
 
-// descreve um nodo da fila de prioridades
-// NAO altere estas estruturas
-struct fpnodo_t
-{
-  void *item ;          // item associado ao nodo
-  int   tipo ;          // tipo do item
-  int   prio ;          // prioridade do item
-  struct fpnodo_t *prox;    // próximo nodo
-};
-
-// descreve uma fila de prioridades
-struct fprio_t
-{
-  struct fpnodo_t *prim ;   // primeiro nodo da fila
-  int num ;         // número de itens na fila
-} ;
-
 /*FUNÇÃO AXILIAR*/
 
 /* confirma se o item procurado já existe na lista.*/
